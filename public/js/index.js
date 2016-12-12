8ac993a8f6705c3dbedc107d0be1a4ef69a93887
Yasser Carreón <yacasia@gmail.com>
2016-12-12 17:18:49 -0600

    Add new and create message event handlers

--- public/js/index.js
+++ public/js/index.js
@@ -1,7 +1,14 @@
 var socket = io()
 socket.on('connect', () => {
     console.log('Connected to server')
+    socket.emit('createMessage', {
+        from: 'Yasser',
+        text: 'Hey, This is Yasser',
+    })
 })
 socket.on('disconnect', () => {
     console.log('Disconnected from server')
+})
+socket.on('newMessage', (message) => {
+    console.log('New message', message)
 })
--- server/server.js
+++ server/server.js
@@ -15,6 +15,17 @@ app.use(express.static(publicPath))
 
 io.on('connection', (socket) => {
     console.log('New user connected')
+
+    socket.emit('newMessage', {
+        from: 'Yasser',
+        text: 'This is a message.',
+        createdAt: new Date()
+    })
+
+    socket.on('createMessage', (message) => {
+        console.log('createMessage', message)
+    })
+
     socket.on('disconnect', () => {
         console.log('User was disconnected')
     })
