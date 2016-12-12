d14159407a03c11e6ac5b8b73d399d627cdf3441
Yasser Carreón <yacasia@gmail.com>
2016-12-12 17:30:37 -0600

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,6 @@
 var socket = io()
 socket.on('connect', () => {
     console.log('Connected to server')
-    socket.emit('createMessage', {
-        from: 'Yasser',
-        text: 'Hey, This is Yasser',
-    })
 })
 socket.on('disconnect', () => {
     console.log('Disconnected from server')
