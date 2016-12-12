dcd74782ce9503a4298b072ecab5e80d230b6c1a
Yasser Carreón <yacasia@gmail.com>
2016-12-12 16:41:06 -0600

    Add socket.io functionality

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const publicPath = path.join(__dirname, '../public')
 const app = express()
 const port = process.env.PORT || 3000
 const server = http.createServer(app) 
+const io = socketIO(server)
 
 app.use(express.static(publicPath))
 
