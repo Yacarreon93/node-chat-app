195dbd5980e44e40f9ef22f934904fea61130ae0
Yasser Carreón <yacasia@gmail.com>
2016-12-13 08:41:30 -0600

    Create from to send message

--- public/js/index.js
+++ public/js/index.js
@@ -8,3 +8,18 @@ socket.on('disconnect', () => {
 socket.on('newMessage', (message) => {
     console.log('New message', message)
 })
+// socket.emit('createMessage', {
+//    from: 'Yasser',
+//    text: 'Hi'
+// }, (data) => {
+//    console.log('Got it', data)
+// })
+$('#message-form').on('submit', (e) => {    
+    e.preventDefault()
+    socket.emit('createMessage', {
+        from: 'User',
+        text: $('[name=message]').val()
+    }, () => {
+        console.log('Got it')
+    })
+})
